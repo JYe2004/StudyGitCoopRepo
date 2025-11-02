90b08f749ad56ffaa7e7810d2386748fe0f504f6
Aurorao777 <tongxuefen777@163.com>
2025-11-02 13:50:33 +0800

    txf_first

---
+++ git_01_springboot/src/main/java/com/zhou/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.zhou.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+
+    @GetMapping("hello")
+    @ResponseBody
+    public String hello() {
+        return "ok";
+    }
+}
